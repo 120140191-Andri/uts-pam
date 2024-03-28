d4befc34c5f21fc4de00444bba19be62d7f0567f
Andri450 <andrisetiawan010101@gmail.com>
2024-03-28 20:06:58 +0700

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,27 +8,9 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var nama: TextView
-    private lateinit var email: TextView
-    private lateinit var alamat: TextView
-    private lateinit var hp: TextView
-    private lateinit var btnKirim: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        nama = findViewById(R.id.nama)
-        email = findViewById(R.id.email)
-        alamat = findViewById(R.id.alamat)
-        hp = findViewById(R.id.hp)
-        btnKirim = findViewById(R.id.button)
-
-        btnKirim.setOnClickListener{
-            val intentTujuan = Intent(this@MainActivity, MainActivity2::class.java);
-            intentTujuan.putExtra(MainActivity2.namadat, nama.text.toString())
-            startActivity(intentTujuan);
-        }
 
     }
 }
