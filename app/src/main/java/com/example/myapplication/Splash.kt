04ab654ceab146746144cb4bd3e99b641e1d6248
Andri450 <andrisetiawan010101@gmail.com>
2024-03-28 22:43:53 +0700

    splash + viewBinding

--- app/src/main/java/com/example/myapplication/Splash.kt
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -1,19 +1,29 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.myapplication.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.logo.alpha = 0.5f
+        binding.logo.animate().setDuration(3000).alpha(1f).withEndAction{
+            Toast.makeText(this, "Oke", Toast.LENGTH_SHORT).show()
+            val intentSplash = Intent(this, Login::class.java)
+            startActivity(intentSplash)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
 
     }
 
