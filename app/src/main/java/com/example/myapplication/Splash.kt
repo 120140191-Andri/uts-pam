00bffd20b262b37603edddf15c0cebc78e03d2d0
Andri450 <andrisetiawan010101@gmail.com>
2024-03-30 00:03:18 +0700

    integrasi firebase

--- app/src/main/java/com/example/myapplication/Splash.kt
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -2,13 +2,16 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Splash : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,15 @@ class Splash : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.i("TAG", currentUser.uid)
+        }else{
+            Log.i("TAG", "kosong")
+        }
 
         tampilLogo(binding)
     }
