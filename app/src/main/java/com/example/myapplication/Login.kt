9655c0c65638c07185e87db6bb002ff6255cb2ec
Andri450 <andrisetiawan010101@gmail.com>
2024-03-30 19:58:45 +0700

    registrasi dan akun auth dan tambah data tambahan ke firestore

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -29,60 +29,14 @@ class Login : AppCompatActivity() {
         }
 
         binding.button2.setOnClickListener {
-            cobaTambahAkun()
+
         }
 
     }
 
     private fun pindahKeRegis(binding: ActivityLoginBinding){
         val elemen = binding.loginLayout
-        elemen.alpha = 1f
-
-        elemen.animate().setDuration(1000).alpha(0f).withEndAction {
-            val intentRegis = Intent(this, Registrasi::class.java)
-            startActivity(intentRegis)
-            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
-            finish()
-        }
-    }
-
-    private fun cobaTambahAkun() {
-        val firestore = FirebaseFirestore.getInstance()
-        Log.i("TAG", "mulai")
-        auth.createUserWithEmailAndPassword("admin", "password")
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    val userData = hashMapOf(
-                        "nama" to "tesnama",
-                        "nim" to "120140191"
-                    )
-                    user?.let {
-                        firestore.collection("users").document(it.uid)
-                            .set(userData)
-                            .addOnSuccessListener {
-                                Toast.makeText(
-                                    this,
-                                    "Registration successful.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            .addOnFailureListener { e ->
-                                Log.i("Gagal", "${e.message}")
-                                Toast.makeText(
-                                    baseContext, "Registration failed. ${e.message}",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                    }
-                } else {
-                    Log.i("Gagal", "${task.exception?.message}")
-                    Toast.makeText(
-                        baseContext, "Registration failed. ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
+        val intentRegis = Intent(this, Registrasi::class.java)
+        startActivity(intentRegis)
     }
 }
--- app/src/main/java/com/example/myapplication/Registrasi.kt
+++ app/src/main/java/com/example/myapplication/Registrasi.kt
@@ -3,12 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityLoginBinding
 import com.example.myapplication.databinding.ActivityRegistrasiBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Registrasi : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistrasiBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,21 +22,105 @@ class Registrasi : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = FirebaseAuth.getInstance()
+
         binding.keLogin.setOnClickListener {
-            pindahKeLogin(binding)
+            pindahKeLogin()
+        }
+
+        binding.registrasiBtn.setOnClickListener {
+            val email = binding.inputEmail.text.toString()
+            val password = binding.inputPassword.text.toString()
+            val usernamegit = binding.inputUsernameGit.text.toString()
+            val nim = binding.inputNIM.text.toString()
+
+            val inputs = mapOf(
+                "email" to email,
+                "password" to password,
+                "usernamegit" to usernamegit,
+                "nim" to nim,
+            )
+
+            val kosong = inputs.filterValues { it.isEmpty() }.keys
+            if(kosong.isEmpty()){
+                ubahStateLoading(true)
+                tambahAkun(inputs)
+            }else{
+                recreate()
+                Toast.makeText(this, "Semua Field Wajib Diisi!", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
 
-    private fun pindahKeLogin(binding: ActivityRegistrasiBinding){
-        val elemen = binding.regisLayout
-        elemen.alpha = 1f
+    private fun pindahKeLogin(){
+        val intentLogin = Intent(this, Login::class.java)
+        startActivity(intentLogin)
+    }
 
-        elemen.animate().setDuration(1000).alpha(0f).withEndAction {
-            val intentLogin = Intent(this, Login::class.java)
-            startActivity(intentLogin)
-            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
-            finish()
+    private fun tambahAkun(inputs: Map<String, String>) {
+
+        auth.createUserWithEmailAndPassword(inputs["email"]!!, inputs["password"]!!)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    tambahDataLain(inputs)
+                    ubahStateLoading(true)
+                } else {
+                    Log.i("Gagal", "${task.exception?.message}")
+                    Toast.makeText(
+                        baseContext, "Registration failed. ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    recreate()
+                    ubahStateLoading(false)
+                }
+            }
+
+    }
+
+    private fun ubahStateLoading(st: Boolean){
+        binding = ActivityRegistrasiBinding.inflate(layoutInflater)
+
+        if (st){
+            binding.registrasiBtn.text = "Loading..."
+        }else{
+            binding.registrasiBtn.text = "Registrasi"
+        }
+    }
+
+    private fun tambahDataLain(inputs: Map<String, String>){
+        val firestore = FirebaseFirestore.getInstance()
+
+        val user = auth.currentUser
+        val userData = hashMapOf(
+            "email" to inputs["email"],
+            "usernamegit" to inputs["usernamegit"],
+            "nim" to inputs["nim"]
+        )
+        user?.let {
+            firestore.collection("users").document(it.uid)
+                .set(userData)
+                .addOnSuccessListener {
+                    ubahStateLoading(false)
+                    Toast.makeText(
+                        this,
+                        "Registration Berhasil, Silahkan Login",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    ubahStateLoading(false)
+                    pindahKeLogin()
+                }
+                .addOnFailureListener { e ->
+                    ubahStateLoading(false)
+                    recreate()
+                    Log.i("Gagal", "${e.message}")
+                    Toast.makeText(
+                        baseContext, "Registration failed. ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    ubahStateLoading(false)
+                }
         }
     }
 
